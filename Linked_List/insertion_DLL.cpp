4d4c7f5dd546e8ca55aaab9269637bb95c2a447c
Priyaanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2024-05-26 18:59:20 +0530

    Add files via upload

---
+++ Linked_List/insertion_DLL.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node *next;
+    Node *back;
+
+
+    Node(int data1, Node* next1, Node* back1){
+       data= data1;
+       next= next1;
+       back= back1;
+
+
+
+    }
+
+
+    Node(int data1){
+        data= data1;
+        next=nullptr;
+        back=nullptr;
+    }
+};
+
+
+Node * convertarr2DLL(vector<int>&arr){
+          Node * Head= new Node(arr[0]);
+          Node *prev = Head;
+
+          for(int i=1;i<arr.size();i++){
+             Node *temp = new Node(arr[i],nullptr,prev);
+            
+            
+               prev->next=temp;
+               prev = temp;// or we can say prev = temp;
+
+
+          }
+
+          return Head;
+
+
+}
+
+void print(Node* Head){
+     Node *temp = Head;
+
+     while(temp!=NULL){
+
+     cout<<temp->data<<" ";
+
+     temp=temp->next;
+}
+
+    cout<<endl;
+} /*void print(Node* Head){
+    while(Head!=NULL){
+        cout<<Head->data<<" ";
+        Head=Head->next;
+    }
+    cout<<endl;
+}*/
+
+
+Node* insertbeforeHead(Node*Head,int val){
+    Node * newhead = new Node(val,Head,nullptr);
+
+    Head->back=newhead;
+
+    return newhead;
+}
+
+int main(){
+
+    vector<int> arr = {2,4,5,8,7,9};
+
+   Node * head = convertarr2DLL(arr);
+
+   print(head);
+
+   cout<<"After Inserting New head"<<endl;
+
+   Node * newhead = insertbeforeHead(head,11);
+
+   print(newhead);
+
+
+
+     
+}
